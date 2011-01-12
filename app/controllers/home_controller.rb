083e3b210c5ef219d2e17d3f6f2fab9ab2364678
Philip Hallstrom <philip@pjkh.com>
2011-01-11 20:38:10 -0800

    extend the demo to show how to use permissions, extractorsw, and archivers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,19 @@ class HomeController < ApplicationController
   def elfinder
     h, r = ElFinder::Connector.new(
       :root => File.join(Rails.public_path, 'system', 'elfinder'),
-      :url => '/system/elfinder'
+      :url => '/system/elfinder',
+      :perms => {
+        'forbidden' => {:read => false, :write => false, :rm => false},
+        /README/ => {:write => false}
+      },
+      :extractors => {
+        'application/zip' => ['unzip', '-qq', '-o'],
+        'application/x-gzip' => ['tar', '-xzf']
+      },
+      :archivers => { 
+        'application/zip' => ['.zip', 'zip', '-qr9'],
+        'application/x-gzip' => ['.tgz', 'tar', '-czf']
+      }
     ).run(params)
 
     headers.merge!(h)
