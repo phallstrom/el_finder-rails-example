a23fcce6f80ccb8aae41f0a0c0b86ad51bcc333c
Evgeny Lapin <lev@openteam.ru>
2011-11-09 10:28:11 +0700

    small fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $('#elfinder').elfinder({
+    url: '/elfinder',
+    lang: 'ru'
+  });
+});
