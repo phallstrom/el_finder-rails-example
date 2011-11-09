1bf149622e305e87d9cb17ceab2d04ccd8cd3acc
Evgeny Lapin <lev@openteam.ru>
2011-11-09 12:10:47 +0700

    update rails to 3.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Railselfinder::Application.config.secret_token = '008b78c0d4dac93a26369c32491ff2397b2e10cfa7c3d2ee64a6bc554602d560ee103e3de93c30391773d13d057dfcefbc1fc049e8fe8383eef7e23aa3fa145d'
+Railselfinder::Application.config.secret_token = '79e46ec33ee1f577367d31050b1f277dabafde5b160094303b6e480726dd8d133f7badef7b50e4764cbd2becd3b21b2eddf2be6b689a30dcc244035f3db0687a'
--- public/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $('#elfinder').elfinder({
-    url: '/elfinder',
-    lang: 'ru'
-  });
-});
