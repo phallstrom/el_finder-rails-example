a85ee470ecbb70b780ed7f96ab15213311996b1f
Philip Hallstrom <philip@pjkh.com>
2011-11-27 13:02:44 -0800

    kill 1.9 hash syntax

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Railselfinder::Application.config.session_store :cookie_store, key: '_railselfinder_session'
+Railselfinder::Application.config.session_store :cookie_store, :key => '_railselfinder_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
